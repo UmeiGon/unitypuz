777ec3931a6d1cbf871a89f18ea000df4ea9f1c4
umei <umeryosan@gmail.com>
2017-09-05 14:44:02 +0900

    aaa

---
+++ BlockPuzzle_Sin/Assets/getHako.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getHako : MonoBehaviour {
+    GameObject player;
+    gamemanager gm;
+    public int zansu;
+    public float getdis;
+    public int tag;
+	// Use this for initialization
+	void Start () {
+        gm = GameObject.Find("GameManager").GetComponent<gamemanager>();
+        player = GameObject.Find("Player");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = (transform.position - player.transform.position).sqrMagnitude;
+        if (Input.GetKeyDown(KeyCode.E)&&gm.horusuu>0&&distance < getdis*getdis)
+        {
+            switch (tag)
+            {
+                case 0:
+                    gm.bluehako++;
+                    break;
+                case 1:
+                    gm.redhako++;
+                    break;
+                case 2:
+                    gm.greenhako++;
+                    break;
+            }
+            zansu--;
+            gm.horusuu--;
+            if (zansu <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- BlockPuzzle_Sin/Assets/script/objSetti.cs
+++ BlockPuzzle_Sin/Assets/script/objSetti.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class objSetti : MonoBehaviour {
 
     GameObject blueCube;
+    public gamemanager gm;
 	// Use this for initialization
 	void Start () {
         blueCube = (GameObject)Resources.Load("prefab/BlueCube", typeof(GameObject));
@@ -15,8 +16,9 @@ public class objSetti : MonoBehaviour {
 	void Update () {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if(Input.GetKeyDown(KeyCode.Mouse0)&&Physics.Raycast(ray,out hit, 100))
+        if(Input.GetKeyDown(KeyCode.Mouse0)&&gm.bluehako>0&&Physics.Raycast(ray,out hit, 100))
         {
+            gm.bluehako--;
             GameObject a = Instantiate(blueCube);
             a.transform.position = hit.point;
         }
